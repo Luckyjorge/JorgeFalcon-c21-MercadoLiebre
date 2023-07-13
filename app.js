d6db277d1d50e4b2c9973283ab1450fdeb50f5ef
LuckyJorge <jorgefalcon71@gmail.com>
2023-07-13 00:07:08 -0300

    add media queries

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const path = require('path');
-const PORT = 3030
+const PORT = 3000
 
 //agregado static
 app.use(express.static('public'));
@@ -15,6 +15,6 @@ app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "404.html"))
 })
 //levantando el server
-app.listen(3030, ()=> {
+app.listen(PORT, ()=> {
     console.log(`TUKI TU SERVER EN http://localhost:${PORT}`)
 })
