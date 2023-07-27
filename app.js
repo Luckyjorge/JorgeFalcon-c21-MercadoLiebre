7358a26d91014b8e0e7c1b56ebdb06f9de1c5a55
LuckyJorge <jorgefalcon71@gmail.com>
2023-07-27 12:43:49 -0300

    add register and login forms

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+const path = require('path');
+const PORT = 3000
+
+//agregado static
+app.use(express.static('public'));
+
+//agregado el home
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "home.html"))
+})
+
+//agregado register.html
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "register.html"))
+})
+//agregado login.html
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "login.html"))
+})
+//agregado 404
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "404.html"))
+})
+//levantando el server
+app.listen(PORT, ()=> {
+    console.log(`TUKI TU SERVER EN http://localhost:${PORT}`)
+})
